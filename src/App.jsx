88702e20480cba8f547cc7cc520fcbc3b5b180f2
Puffywonton <wontonking123@outlook.com>
2023-11-09 17:29:38 +0100

    working demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {Icon} from 'leaflet'
+import MarkerClusterGroup from 'react-leaflet-cluster'
+import { useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markers, setMarkers] = useState([
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "caca 1",
+      img: "/public/pictures/macaron.jpg"
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: "caca 2",
+      img: "/public/pictures/zemmour.jpg"
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "caca 3",
+      img: "/public/pictures/sardine.jpg"
+    },
+  ])
+
+
+  const customIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/4063/4063526.png ",
+    iconSize: [38, 38]
+  })
+
+  const addCaca = () => {
+    setMarkers([
+      ...markers,
+      {
+        geocode: [48.845, 2.3545],
+        popUp: "nouveau caca",
+        img: "/public/pictures/merluche.jpg"
+      }
+    ]) 
+    console.log('added caca')
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>La carte des cacas de Paris</h1>
+      <button onClick={addCaca}>Ajouter un caca</button>
+      <MapContainer className='map' center={[48.8566, 2.3522]} zoom={10}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <MarkerClusterGroup
+          chunkedLoading
+        >
+          {markers.map((marker, index) => (
+            <Marker position={marker.geocode} key={index} icon={customIcon}>
+              <Popup>
+                <p>{marker.popUp}</p>
+                <img src={marker.img}></img>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
     </>
   )
 }
